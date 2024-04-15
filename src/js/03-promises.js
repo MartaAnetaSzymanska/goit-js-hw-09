eba4130d2658502204cf629ba925f6c89a402b2f
MartaAnetaSzymanska <szymanska.ma@gmail.com>
2024-04-15 17:51:11 +0200

    function_create-promise

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,21 +1,33 @@
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`Fulfilled promise`);
+      } else {
+        reject(`Rejected promise`);
+      }
+    }, delay);
+  });
+  promise.then(({ position, delay }) => {
     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  } else {
+  });
+  promise.catch(({ position, delay }) => {
     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  }
+  });
 }
+
 const promiseForm = document.querySelector('.form');
-let timerId;
+let position = 1;
 
 promiseForm.addEventListener('submit', ev => {
+  ev.preventDefault();
   let delay = ev.target.elements.delay.value;
   let step = ev.target.elements.step.value;
   let amount = ev.target.elements.amount.value;
 
-  for (let position = 1; position <= amount; position++) {
-    timerId = setTimeout(createPromise(position, delay));
+  for (position; position <= amount; position++) {
+    createPromise();
     delay += step;
   }
 });
