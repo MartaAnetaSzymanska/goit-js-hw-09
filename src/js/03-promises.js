8c4a95ab5b071f2775036811dc3d72c01c20098a
MartaAnetaSzymanska <szymanska.ma@gmail.com>
2024-04-15 16:16:48 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,21 @@
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    // Reject
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
   }
 }
+const promiseForm = document.querySelector('.form');
+let timerId;
+
+promiseForm.addEventListener('submit', ev => {
+  let delay = ev.target.elements.delay.value;
+  let step = ev.target.elements.step.value;
+  let amount = ev.target.elements.amount.value;
+
+  for (let position = 1; position <= amount; position++) {
+    timerId = setTimeout(createPromise(position, delay));
+    delay += step;
+  }
+});
